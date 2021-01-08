9bd5e7e7c63a8673aa42523129ae13dc4050664a
Akosile Tayo <akosilety9@gmail.com>
2021-01-08 02:21:24 +0100

    Started front end job

---
+++ script.js
@@ -0,0 +1,43 @@
+// const app = new Vue({
+//     el: '#app',
+//     data: {
+//         message: 'You loaded this message on ' + new Date().toLocaleString()
+//     }
+// })
+
+// const app2 = new Vue({
+//     el: '#app-2',
+//     data: {
+//         seen: true
+//     }
+// })
+// const app6 = new Vue({
+//     el: '#app-6',
+//     data: {
+//         message: "Created a Vue Template"
+
+//     }
+// })
+
+// async function getData() {
+//     const res = await fetch('https://api.agify.io/?name=michael');
+//     const data = await res.json();
+//     this.data = data;
+//     console.log(data)
+// }
+
+// Vue.component('todo-item', {
+//     props: ['todo'],
+//     template: '<li>{{todo.text}}</li>'
+
+// })
+// var app7 = new Vue({
+//     el: '#app-7',
+//     data: {
+//         groceryList: [
+//             { id: 0, text: 'Vegetable' },
+//             { id: 1, text: "Rice" },
+//             { id: 1, text: "Rice" }
+//         ]
+//     }
+// });
